a7473347d71020751dd377f81fb9f7782a6759f5
shahbaz ramzan <mshahbazramzan952@gmail.com>
2023-02-25 16:24:10 +0500

    message

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
-
-import './App.css';
-import Navbar from './Components/Navbar';
-import { BrowserRouter ,Route,Routes } from 'react-router-dom';
-import Browse from './Pages/Browse';
-import Home from './Pages/Home';
-import Blog from './Pages/Blog';
-import Help from './Pages/Help';
-import Jobs from './Pages/Jobs';
-import WhitePaper from './Pages/WhitePaper';
-import Login from './Pages/Login';
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Browse from "./Pages/Browse";
+import Home from "./Pages/Home";
+import Blog from "./Pages/Blog";
+import Help from "./Pages/Help";
+import Jobs from "./Pages/Jobs";
+import WhitePaper from "./Pages/WhitePaper";
+import Login from "./Pages/Login";
 function App() {
   return (
     <div className="App">
-    
-   <BrowserRouter>
-   <Navbar />
-   <Routes>
-   <Route path='/' element={<Home/>}/>
-   <Route path='/Browse' element={<Browse/>}/>
-   <Route path='/Blog' element={<Blog/>}/>
-   <Route path='/Help' element={<Help/>}/>
-   <Route path='/Jobs' element={<Jobs/>}/>
-   <Route path='/whitePaper' element={<WhitePaper/>}/>
-   <Route path='/Login' element={<Login/>} />
-   </Routes>
-   </BrowserRouter>
-
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Browse" element={<Browse />} />
+          <Route path="/Blog" element={<Blog />} />
+          <Route path="/Help" element={<Help />} />
+          <Route path="/Jobs" element={<Jobs />} />
+          <Route path="/whitePaper" element={<WhitePaper />} />
+          <Route path="/Login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,32 +1,57 @@
 import { Link } from "react-router-dom";
 import "./Style.css";
+import { useEffect, useState } from "react";
 
 function Login() {
+  const [email,setEmail] =useState('');
+  const [password,setPassword] =useState('');
+ 
+  const handleSubmit=(event)=>{
+    event.preventDefault();
+    console.log("Email:",email);
+    console.log("password:",password);
+
+
+  }
+  useEffect
+
+
+
+
   return (
     <div className="form">
       <h1>Login To App</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="container">
-          <label>Email*</label><br></br>
-          <input type="text" name="email" placeholder="Email"></input>
+          <label>Email*</label>
+          <br></br>
+          <input type="text" value={email} placeholder="Email" onChange={(event) => setEmail(event.target.value)} />
         </div>
         <div className="container">
-          <label>Password*</label><br></br>
-          <input type="password" name="password" placeholder="Password" />
+          <label>Password*</label>
+          <br></br>
+          <input type="password"  placeholder="Password" value={password} onChange={(event) => setPassword(event.target.value)}/>
         </div>
-        <div style={{marginTop:"30px"}}><span style={{textAlign:"left" ,margin:"20px"}} >
-        <input type="checkbox" name="Remember Me" />
-        <label>Remember Me</label>
-        </span>
-        <Link>ForgotPassword</Link>
+        <div style={{ marginTop: "30px" }}>
+          <span style={{ textAlign: "left", margin: "20px" }}>
+            <input type="checkbox" name="Remember Me" />
+            <label>Remember Me</label>
+          </span>
+          <Link to="#">ForgotPassword</Link>
         </div>
-        <div>
+        <div></div>
 
-        </div>
-        
         <div className="container">
           <button>Login </button>
         </div>
+        <br></br>
+        <p>Or</p>
+        <br></br>
+        <div>
+          <button>Request One Time Verification Code</button>
+        </div>
+        <br></br>
+        <p>Login with social accounts</p>
       </form>
     </div>
   );
